2d811181398e3c652adeb51ee7be4392b5c97897
BG <BG@DESKTOP-AQPEC83>
2023-06-22 19:53:34 +0300

    chore: first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function hideobject(clickobject) {
+    var object = document.getElementById(clickobject);
+    if (object.style.display === "none") { object.style.display = "block"; }
+    else { object.style.display = "none"; }
+  }
